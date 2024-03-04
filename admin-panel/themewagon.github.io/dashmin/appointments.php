ca49edef779c04ad80a3d6d1e37013559eab4a8d
asim <asimusman8899@gmail.com>
2024-03-03 20:47:38 -0800

    initial comment

--- admin-panel/themewagon.github.io/dashmin/appointments.php
+++ admin-panel/themewagon.github.io/dashmin/appointments.php
@@ -119,9 +119,11 @@ if ($result->num_rows > 0) {
                     </div>
                     <a href="widget.php" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Widgets</a>
                     <a href="form.php" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i>Forms</a>
-                    <a href="table.php" class="nav-item nav-link active"><i class="fa fa-table me-2"></i>Tables</a>
-                    <a href="category.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Add Category</a>
+                    <a href="table.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Tables</a>
+                    <a href="category.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Add category</a>
                     <a href="addproduct.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Add product</a>
+                    <a href="tester.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Add Tester</a>
+                    <a href="appointments.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Appointments</a>
                     <a href="chart.php" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Charts</a>
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i
